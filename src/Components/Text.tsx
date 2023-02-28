51bd5dfd173cca71a15e80a80198c2d0eab48c2e
gguilhermelopes <guilhermelopes275@gmail.com>
2023-02-28 14:55:04 -0300

    refactor: adding a generic type to our component

--- src/Components/Text.tsx
+++ src/Components/Text.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-const Text = <C extends React.ElementType>({
-  as,
-  children,
-}: {
+type TextProps<C extends React.ElementType> = {
   as?: C;
   children: React.ReactNode;
-}) => {
+};
+
+const Text = <C extends React.ElementType>({ as, children }: TextProps<C>) => {
   const Component = as || "span";
   return <Component>{children}</Component>;
 };
