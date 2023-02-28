addfb8f639f3fe19bbda0a6effd67cd0fd475480
gguilhermelopes <guilhermelopes275@gmail.com>
2023-02-28 15:31:36 -0300

    feat: using the component with a custom component in "as"

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import "./App.css";
 import Text from "./Components/Text";
 
+const Emphasis = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <em style={{ background: "yellow", color: "black", fontSize: "4rem" }}>
+      {children}
+    </em>
+  );
+};
+
 function App() {
   return (
     <div className="App">
       <Text as="h1">Hello</Text>
       <Text as="a" href="https://www.google.com">
         Google!
-      </Text>
+      </Text>{" "}
+      <br />
+      <Text as={Emphasis}>This is important!</Text>
     </div>
   );
 }
